3feaae3df6d53e7b0e8ab576c14dca046f66539e
Ruben Ordonez <ruben@ordonez.us>
2022-08-20 02:30:02 -0700

    Updated Employees Index

--- Pages/Employees/Index.cshtml.cs
+++ Pages/Employees/Index.cshtml.cs
@@ -14,6 +14,9 @@ namespace PhoneDirectory.Pages.Employees
     {
         private readonly PhoneDirectory.Data.DirectoryContext _context;
 
+        public string CurrentFilterPhone {get; set;}
+        public string CurrentFilterName { get; set;}
+
         public IndexModel(PhoneDirectory.Data.DirectoryContext context)
         {
             _context = context;
@@ -21,15 +24,35 @@ namespace PhoneDirectory.Pages.Employees
 
         public IList<Employee> Employee { get;set; } = default!;
 
-        public async Task OnGetAsync()
+        public async Task<IActionResult> OnGetAsync(string searchType, string searchString)
         {
-            if (_context.Employees != null)
+        
+            IQueryable<Employee> employeesIQ = _context.Employees.Include(d => d.Department);
+                                                                
+            if (!String.IsNullOrEmpty(searchString))
             {
-                Employee = await _context.Employees
-                    .Include( d => d.Department)
-                    .ToListAsync();
+                switch (searchType)
+                {
+                    case "name":
+                        employeesIQ = employeesIQ.Where(s => s.LastName.Contains(searchString)
+                                    || s.FirstName.Contains(searchString));
+                        CurrentFilterName = searchString;
+                        break;
+                    case "phone":
+                        employeesIQ = employeesIQ.Where(s => s.Phone1.Contains(searchString)
+                                    || s.Phone2.Contains(searchString)
+                                    || s.Ext.Contains(searchString));
+                        CurrentFilterPhone = searchString;
+                        break;
+                    default:
+                        break;
+                } 
                 
             }
+
+            Employee = await employeesIQ.AsNoTracking().ToListAsync();
+                
+            return Page();
         }
     }
 }
